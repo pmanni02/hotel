d953dc48708b8f3266ff0b0ff9d68223b428ce64
Phoebe <phoebe.manning8@gmail.com>
2018-03-05 15:49:02 -0800

    Adds #initialize and related tests in Reservation class

--- lib/reservation.rb
+++ lib/reservation.rb
@@ -1,5 +1,15 @@
 module Hotel
   class Reservation
 
+    attr_reader :start_date, :end_date, :cost, :room_id, :reservation_id
+
+    def initialize(date_range)
+      @start_date = date_range[:start_date]
+      @end_date = date_range[:end_date]
+      @cost = 0.0
+      @room_id = 0
+      @reservation_id = 0
+    end
+
   end
 end
--- specs/reservation_spec.rb
+++ specs/reservation_spec.rb
@@ -1,5 +1,25 @@
 require_relative 'spec_helper'
 
-descibe "Reservation class" do
-  
+describe "Reservation class" do
+  before do
+    date_range = {
+      start_date: Date.new(2018, 03, 05),
+      end_date: Date.new(2018, 03, 10)
+    }
+    @reservation = Hotel::Reservation.new(date_range)
+  end
+
+  describe "Initializer" do
+    it "is an instance of Reservation" do
+      @reservation.must_be_instance_of Hotel::Reservation
+    end
+
+    it "correctly initializes instance variables" do
+      @reservation.start_date.must_be_kind_of Date
+      @reservation.end_date.must_be_kind_of Date
+      @reservation.cost.must_be_kind_of Float
+      @reservation.room_id.must_be_kind_of Integer
+      @reservation.reservation_id.must_be_kind_of Integer
+    end
+  end
 end
