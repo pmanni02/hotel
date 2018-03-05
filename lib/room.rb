0b1d255722271457be63901514c5c15a8a2537aa
Phoebe <phoebe.manning8@gmail.com>
2018-03-05 14:01:58 -0800

    Adds #initialize and tests

--- lib/room.rb
+++ lib/room.rb
@@ -1,5 +1,12 @@
 module Hotel
   class Room
 
+    attr_reader :is_reserved, :room_id
+
+    def initialize(id)
+      @is_reserved = FALSE
+      @room_id = id
+    end
+
   end
 end
--- specs/room_spec.rb
+++ specs/room_spec.rb
@@ -1,5 +1,19 @@
 require_relative 'spec_helper'
 
 describe "Room class" do
-  
+  before do
+    id = 1
+    @room = Hotel::Room.new(id)
+  end
+
+  describe "Initializer" do
+    it "is an instance of Room" do
+      @room.must_be_instance_of Hotel::Room
+    end
+
+    it "creates initial data structures" do
+      @room.is_reserved.must_equal FALSE
+      @room.room_id.must_be_kind_of Integer
+    end
+  end
 end
